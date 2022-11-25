aa97ea1fe275c35f73f609f60f7c52191dbbefdf
jjuhea <114230836+jjuhea@users.noreply.github.com>
2022-11-25 14:01:32 +0900

    333

--- sketch.js
+++ sketch.js
@@ -1,16 +1,3 @@
-// let mySound;   //변수명 my Sound
-//     function setup(){
-//         soundFormats('YOUNHA.mp3');
-//         mySound=loadSound('YOUNHA',loadMusic); //mySound 변수에 음악을 넣는다.
-//         musicFile.play
-//     }
-
-
-// function loadMusic()
-// {
-//     mySound.play();
-// }
-
 var song;
 var button;
 
