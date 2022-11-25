be9b56df6e4338125620715f1882cb9765d48317
jjuhea <114230836+jjuhea@users.noreply.github.com>
2022-11-25 13:09:40 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+let mySound;   //변수명 my Sound
+    function setup(){
+        soundFormats('YOUNHA.mp3');
+        mySound=loadSound('YOUNHA.mp3',loadMusic); //mySound 변수에 음악을 넣는다.
+        musicFile.play
+    }
+
+
+function loadMusic()
+{
+    mySound.play();
+}
+
+
